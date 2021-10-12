32463e0fb8dc07b1a2d302d171e890cd00d5c985
Briana-Koo <RoyalBlueSnow@gmail.com>
2021-10-12 11:00:20 -0500

    Initial Commit

---
+++ Multiply Take 2/Multiply_Take_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Multiply_Take_2App.swift
+//  Multiply Take 2
+//
+//  Created by Student on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Multiply_Take_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Multiply Take 2Tests/Multiply_Take_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Multiply_Take_2Tests.swift
+//  Multiply Take 2Tests
+//
+//  Created by Student on 9/14/21.
+//
+
+import XCTest
+@testable import Multiply_Take_2
+
+class Multiply_Take_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
